87fa1b71db54610dc577cdc2d60cff995f900063
Felipe-ABS <FelipeABS2@Gmail.com>
2024-02-05 11:31:49 -0300

    Aprender sobre as chaves e as funções do JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
 function App() {
+  const name = "Felipe"
+  const newName = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const url = "https://via.placeholder.com/150"
+
   return (
     <div className="App">
-      <h1>Olá React</h1>
-      <p>Primeira aplicação React</p>
+      <h1>Alterando JSK</h1>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha Imagem"/>
     </div>
   );
 }
