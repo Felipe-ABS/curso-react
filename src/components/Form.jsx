505a5fb6185a9343f3ccfb6c169c22018fc007b2
Felipe-ABS <FelipeABS2@Gmail.com>
2024-02-09 16:25:51 -0300

    Aprender a utilização de métodos por meio de props através de componentes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,45 @@
+import { useState } from "react";
+
 function Form() {
 
     function cadastrarUsuario(e) {
         e.preventDefault()
-        console.log('Cadastrou Usuário!');
+        console.log(`Usuário "${name}" foi cadastrado com a senha "${password}"`)
     }
+
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState()
 
     return(
         <div>
             <h1>Meu Cadastro</h1>
+            <h2>Nome Digitado: {name}</h2>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome"/>
+                    <label htmlFor="name">Nome: </label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Digite o seu nome"
+                        onChange={(teste) => setName(teste.target.value)}
+                    />
                 </div>
                 <div>
-                    <input type="submit" value="Cadastrar!"/>
+                    <label htmlFor="password">Senha: </label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Digite a sua senha"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="submit"
+                        value="Cadastrar!"
+                    />
                 </div>
             </form>
         </div>
